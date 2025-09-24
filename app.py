f3cf4904d5e2a4d9d4aed6cd7bdee5209bfa3941
Ирина Андреева <rbbzzns@gmail.com>
2025-09-24 21:41:25 +0700

    Имя студента

--- app.py
+++ app.py
@@ -326,4 +326,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Ирина Андреева'
+    return render_template('example.html', name=name)
