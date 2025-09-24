e20f064ca54e9cd6ab4e75109d74004471598d1a
Ирина Андреева <rbbzzns@gmail.com>
2025-09-24 23:15:21 +0700

    Условия в шаблоне

--- app.py
+++ app.py
@@ -329,13 +329,11 @@ def example():
     name = 'Ирина Андреева'
     lab_number = 2
     group = 'ФБИ-33'
-    course = 3
-    year = 2025
+    course = '3 курс'
     return render_template(
         'example.html',
         name=name,
         lab_number=lab_number,
         group=group,
         course=course,
-        year=year
     )
