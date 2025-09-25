f827f5cfe61f979695f4741b5da98ed7f7e38d3b
Ирина Андреева <rbbzzns@gmail.com>
2025-09-25 09:55:41 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -339,3 +339,7 @@ def example():
                             lab_number=lab_number,
                             group=group,
                             course=course, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
