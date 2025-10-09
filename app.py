fcbccf4ca4e72860e58bc5528b5fcf38ee8f0b5e
Ирина Андреева <rbbzzns@gmail.com>
2025-10-09 23:05:45 +0700

    Заготовка к третьей лабораторной работе

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 
 error_log = []
@@ -25,7 +27,8 @@ def index():
         <h1>НГТУ, ФБ, WEB-программирование, часть 2.<br> Список лабораторных</h1>
         <ul>
             <li><a href="/lab1">Первая лабораторная</a></li>
-            <li><a href="/lab2">Вторая лабораторная</a></i>
+            <li><a href="/lab2">Вторая лабораторная</a></li>
+            <li><a href="/lab3">Третья лабораторная</a></li>
         </ul>
         <hr>
         <footer>
