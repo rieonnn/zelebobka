45b1eed863b8aa51edd02e1d0eeff1d43294779f
Ирина Андреева <rbbzzns@gmail.com>
2025-10-23 09:43:31 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import datetime
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
+
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 
 error_log = []
@@ -29,6 +32,7 @@ def index():
             <li><a href="/lab1">Первая лабораторная</a></li>
             <li><a href="/lab2">Вторая лабораторная</a></li>
             <li><a href="/lab3">Третья лабораторная</a></li>
+            <li><a href="/lab4">Четвертая лабораторная</a></li>
         </ul>
         <hr>
         <footer>
@@ -122,5 +126,3 @@ def internal_error(err):
     </body>
 </html>
 ''', 500
-
-
