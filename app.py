fadf6a9e13e24c1b99e501800a694761e6bf4bb2
Ирина Андреева <rbbzzns@gmail.com>
2025-11-26 14:41:31 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
-from lab6 import lab6_bp   # исправлено!
+from lab6 import lab6_bp
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -20,7 +21,8 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
-app.register_blueprint(lab6_bp)   # исправлено!
+app.register_blueprint(lab6_bp)
+app.register_blueprint(lab7)
 
 error_log = []
 
@@ -44,6 +46,8 @@ def index():
             <li><a href="/lab4">Четвертая лабораторная</a></li>
             <li><a href="/lab5">Пятая лабораторная</a></li>
             <li><a href="/lab6">Шестая лабораторная</a></li>
+            <li><a href="/lab7">Седьмая лабораторная</a></li>
+            <li><a href="/rgz">РГЗ</a></li>
         </ul>
         <hr>
         <footer>
