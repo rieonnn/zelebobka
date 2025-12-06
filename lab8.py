f6d6d0e9133b10520e40c37d1ed0061ee18b6ee9
Ирина Андреева <rbbzzns@gmail.com>
2025-12-06 11:30:03 +0700

    Модель базы данных

--- lab8.py
+++ lab8.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template
 
 lab8 = Blueprint('lab8', __name__)
 
-DATABASE = 'lab.db'
-
 @lab8.route('/lab8/')
 def main():
     return render_template('lab8/lab8.html')
