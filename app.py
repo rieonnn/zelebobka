3d20493896d9eaaaefcafd541aaff110cfbb078b
Ирина Андреева <rbbzzns@gmail.com>
2025-12-20 19:38:04 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ from lab5 import lab5
 from lab6 import lab6_bp
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz_bp
 
 app = Flask(__name__)
@@ -56,6 +57,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6_bp)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz_bp)
 
 error_log = []
@@ -82,6 +84,7 @@ def index():
             <li><a href="/lab6">Шестая лабораторная</a></li>
             <li><a href="/lab7">Седьмая лабораторная</a></li>
             <li><a href="/lab8">Восьмая лабораторная</a></li>
+            <li><a href="/lab9">Девятая лабораторная</a></li>
             <li><a href="/rgz">РГЗ</a></li>
         </ul>
         <hr>
